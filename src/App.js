b52a9330c6a2224a99580772d07fb1937ff99140
briandriscollcode <bdriscoll407@gmail.com>
2021-12-13 14:24:30 -0700

    cleaned up basic React to start documentation app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Phaser Documentation </h1>
       </header>
+
+      <section className="App-body">
+          
+          <navigation className="App-subjects"> </navigation>
+          <main>Hi</main>
+          <navigation className="App-headers"> </navigation>
+
+      </section>
     </div>
   );
 }
